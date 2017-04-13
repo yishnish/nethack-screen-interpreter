d5d84f4b68df574685adaa7d94e78d1458bbcdee
yishnish <yishnish@gmail.com>
2017-04-13 19:28:05 +0000

    some hashcode testing

--- src/test/java/locations/CoordinatesTest.java
+++ src/test/java/locations/CoordinatesTest.java
@@ -2,6 +2,10 @@ package locations;
 
 import org.junit.Test;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.hamcrest.core.IsNot.not;
@@ -26,5 +30,21 @@ public class CoordinatesTest {
         assertThat(coordinates1, equalTo(coordinates3));
 
         assertThat(coordinates3, equalTo(coordinates1));
+    }
+
+    @Test
+    public void testSameCoordinatesHashTheSame(){
+        Coordinates coordinates1 = new Coordinates(1, 1);
+        Coordinates coordinates2 = new Coordinates(1, 2);
+        Coordinates coordinates3 = new Coordinates(1, 1);
+
+        Set<Coordinates> distinctCoordinates = new HashSet<Coordinates>();
+        distinctCoordinates.add(coordinates1);
+        distinctCoordinates.add(coordinates2);
+        distinctCoordinates.add(coordinates3);
+
+        assertThat(distinctCoordinates.size(), equalTo(2));
+        assertThat(distinctCoordinates, hasItem(coordinates1));
+        assertThat(distinctCoordinates, hasItem(coordinates2));
     }
 }
